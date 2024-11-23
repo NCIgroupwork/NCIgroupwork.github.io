2f62c3f73e035ea6fd5e1a8b47c780aae2161478
NCIgroupwork <nci.work24@gmail.com>
2024-11-23 18:58:35 +0000

    Group project
    Adding Services and FAQ pages. Fixed navigation bar to allow collapse on the smaller screen

---
+++ Java Fitness/Javascript/script.js
@@ -0,0 +1,13 @@
+/*
+Group project
+*/
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
