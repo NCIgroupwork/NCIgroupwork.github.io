07c45acc2db35a89594a62ddc4293be72ebe896f
NCIgroupwork <nci.work24@gmail.com>
2024-11-26 23:45:27 +0000

    Update javascript to validate the form
    Update javascript to validate the form and display sign up message.

--- Java Fitness/Javascript/script.js
+++ Java Fitness/Javascript/script.js
@@ -11,3 +11,33 @@ if (toastTrigger) {
     toastBootstrap.show()
   })
 }
+
+function signupmsg(event){
+    
+  event.preventDefault();
+
+  const form = document.querySelector(".form-wrapper form");
+
+  //verify input format
+  if (!form.checkValidity()) {
+    form.reportValidity();
+    return;
+  }
+
+  let x = document.getElementById("inputName").value;
+
+  let y = document.getElementById("inputEmail").value;
+
+  let z = document.getElementById("inputState").value;
+
+  //Verify service box
+  if (z === "Choose a service") {
+    alert("Please fill out all fields and select a valid service.");
+    return;
+  }
+  
+  document.getElementById("signupform").style.display = "none";
+
+  document.getElementById("message").innerHTML = `${x}, thank you for your details.<br> We will be in touch via ${y} shortly regarding your sign up request for ${z} service.`;  
+
+}
