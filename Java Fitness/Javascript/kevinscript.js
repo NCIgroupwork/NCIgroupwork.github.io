534218e3fca380e0a6109e59f434ef402c6f96d1
KCasey94 <x24164739@student.ncirl.ie>
2024-12-21 13:20:39 +0000

    1

--- Java Fitness/Javascript/kevinscript.js
+++ Java Fitness/Javascript/kevinscript.js
@@ -3,4 +3,10 @@ function signupmsg(event){
     event.preventDefault();
   
     const form = document.querySelector(".form-wrapper form");
+  
+    //verify input format
+    if (!form.checkValidity()) {
+      form.reportValidity();
+      return;
+    }
 }
